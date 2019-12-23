abe5ebd7b995d8e22ea384cd9d325500b72735df
nicolerockower <njrockower@gmail.com>
2019-12-22 19:39:49 -0500

    Updated listing details content and styling

--- src/components/ListingDetail.js
+++ src/components/ListingDetail.js
@@ -36,10 +36,35 @@ class ListingDetail extends Component {
 					<NavHeader show_logo={false}/>
 				</div>
 				<div id='image-container'>
+					<div id='left-img'>
+					</div>
+					<div id='right-img'>
+					</div>
 				</div>
 				<div id='center-container'>
-				<div id='listing-description-container'></div>
-				<div id='book-listing-container'></div>
+				<div id='listing-description-container'>
+					<div id='description-top-section'>
+						<div id='description-title'>
+							<h1>{listing.name}</h1>
+						</div>
+						<div id='description-profile'>
+								<img src='https://i.imgur.com/0HuzCyw.png' alt={''} />
+								<br/>
+								{listing.host_name}
+						</div>
+					</div>
+					<br/>
+					{listing.city}
+					<br/>
+					<br/>
+					{listing.type}
+					<div className='description-section'>
+						{listing.description}
+					</div>
+				</div>
+				<div id='book-listing-container'>
+
+				</div>
 				</div>
 			</div>
 		);
