2324029515e4f67f7d3111062c41771688a7e79e
nicolerockower <njrockower@gmail.com>
2020-01-20 17:43:27 -0500

    Add auth components

---
+++ src/components/withAuth.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import AuthService from './AuthService';
+
+export default function withAuth(AuthComponent) {
+	const Auth = new AuthService('http://localhost:8080');
+
+	return class AuthWrapped extends Component {
+		constructor() {
+			super();
+
+			this.state = {
+				user: null
+			};
+		}
+
+		componentDidMount() {
+			if (!Auth.loggedIn()) {
+				console.log('not logged in');
+
+				this.props.history.push('/login');
+			}
+			else {
+				try {
+					const profile = Auth.getProfile();
+
+					this.setState({
+						user: profile
+					});
+				}
+				catch(err){
+					Auth.logout();
+					this.props.history.push('/login');
+				}
+			}
+		}
+
+		render() {
+			if (this.state.user) {
+				return (
+					<AuthComponent history={this.props.history} user={this.state.user} />
+				)
+			}
+			else {
+				return null;
+			}
+		}
+	}
+}
