5b5c0172d2e5611f4d6400a8cb5efc05715f6534
Nicole Rockower <nicolerockower@Nicoles-MacBook-Air-2.local>
2020-02-17 13:52:25 -0500

    Remove unused import

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,6 @@
 import React, { Component }  from 'react';
 import BeatLoader from 'react-spinners/BeatLoader';
-import { css } from '@emotion/core';
 import '../styles/Listings.css';
-
-const override = css`
-    display: block;
-    border-color: red;
-`;
 
 class Spinner extends Component {
 	constructor(props) {
@@ -20,7 +14,6 @@ class Spinner extends Component {
 			<div className='listings-container'>
 				<div className='results-container'>
 					<BeatLoader
-						css={override}
 						sizeUnit={'px'}
 						size={15}
 						color={'#e0e0e0'}
