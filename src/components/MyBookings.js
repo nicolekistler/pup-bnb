20b1196ce25ed38cb1082713b331e3ae1c683ded
Nicole Rockower <nicolerockower@Nicoles-MacBook-Air-2.local>
2020-02-17 20:36:07 -0500

    Added method to get bookings

--- src/components/MyBookings.js
+++ src/components/MyBookings.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import NavHeader from './NavHeader';
 import withAuth from './withAuth';
 import AuthService from '../services/AuthService';
+import BookingService from '../services/BookingService';
 import '../styles/MyBookings.css';
+import Spinner from './Spinner';
 import tripsIllustration from '../assets/trips-illustration.jpg'
 
 class MyBookings extends Component {
@@ -12,25 +14,43 @@ class MyBookings extends Component {
 
 		document.title = `Upcoming • Pupbnb`;
 
+		this.state = {
+			bookings: '',
+			fetchInProgress: true
+		};
+
 		this.Auth = new AuthService();
+		this.Booking = new BookingService();
 
 		this.userId = this.Auth.getUserId();
 	}
+
+	componentDidMount() {
+		this.Booking.getBookings()
+			.then(data => {
+				this.setState({
+					bookings: data.booking,
+					fetchInProgress: false
+				});
+			});
+	}
 	
 	render() {
+		console.log(this.state);
+
+		const results =
+			<div className='upcoming-trip-container'>
+				Testing 123
+			</div>;
+
 		return (
 			<div>
 				<NavHeader/>
 				<div id='my-bookings-container'>
 					<div id='upcoming-container'>
-						<h2>Hello, Andrea!</h2>
+						<h2>Hello, Name!</h2>
 						Check out your upcoming trips
-						<div className='upcoming-trip-container'>
-						Testing 123
-						</div>
-						<div className='upcoming-trip-container'>
-						Testing 123
-						</div>
+						{ this.state.fetchInProgress ? <Spinner /> : results }
 					</div>
 					<img alt={''} src={tripsIllustration}/>
 				</div>
